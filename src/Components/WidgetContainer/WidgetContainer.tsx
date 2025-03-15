8aac3e4eea2b068a08a890710cb8c09df1d2490e
christiantumandao <christianptumandao@gmail.com>
2025-03-15 00:20:49 -0400

    Fixed search results and exiting it

--- src/Components/WidgetContainer/WidgetContainer.tsx
+++ src/Components/WidgetContainer/WidgetContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./WidgetContainer.css";
 import { location, userData, Weather } from "../../util/types";
 import WidgetBody from "../WidgetBody/WidgetBody";
@@ -26,6 +26,7 @@ const WidgetContainer = (props: WidgetContainerProps) => {
     const [queryResults, setQueryResults] = useState<location[] | undefined>(undefined);
     const { userData, setUsersRegions, usersRegions, currLocation, setCurrLocation } = props;
     const [user] = useAuthState(auth);
+    const formRef = useRef<HTMLFormElement | null>(null);
 
     const [weather, setWeather] = useState<Weather | null>(null);
 
@@ -48,6 +49,21 @@ const WidgetContainer = (props: WidgetContainerProps) => {
         
     },[currLocation, userData.units]);
 
+    // 
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (formRef.current && !formRef.current.contains(event.target as Node)) {
+                setQueryResults(undefined); 
+                setRegionQuery("");
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
 
     const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -57,74 +73,89 @@ const WidgetContainer = (props: WidgetContainerProps) => {
         } catch (e) {
             console.error(e);
         }
-
-        
-        setRegionQuery("");
     }
 
     const handleSelectLocation = async (loc: location) => {
         setCurrLocation(loc);
         setQueryResults(undefined);
+        setRegionQuery("");
         const weatherData: Weather | null = await fetchWeather(loc.lat.toString(), loc.lon.toString(), userData.units);
         setWeather(weatherData);
     }
 
+    const getSearchBar = () => {
+        return (
+            <form 
+            onSubmit = { handleSearch }
+            ref = { formRef }
+            className={(queryResults) ? "search-region-container search-region-container-results" : "search-region-container"}
+            >
+                <input 
+                    className="search-bar"
+                    type="text"
+                    placeholder="Region"
+                    value = { regionQuery }
+                    onChange = { (e: React.ChangeEvent<HTMLInputElement>) => setRegionQuery(e.target.value)}
+                />
+                <button type='submit' className={(regionQuery.length > 0) ? "search-btn" : "search-btn display-none"}><CiSearch /></button>
+                {
+                    (queryResults) ? 
+                        <ul className="query-results-container">
+                            {
+                                queryResults.map((loc,idx) => (
+                                    <li
+                                        onClick = { () => handleSelectLocation(loc) }
+                                        key = { idx+ loc.lat }>
+                                            {loc.name + ", " + ( (loc.state) ? loc.state : countries.getName(loc.country,"en"))}
+                                    </li>
+                                    ))
+
+                            }
+                        </ul>
+                
+                        : null
+                }
+            </form>   
+        )
+    }
+
+    const getHeader = () => {
+        return (
+            <header className="Widget--header">
+                {
+                    (user)
+                    ? <h2>Hello, { userData.firstName }</h2>
+                    : <h2>Hello</h2>
+                }
+
+                { getSearchBar() }
+                        
+
+                <button
+                    className="regions-weather-button"
+                    onClick = { ()=> nav("/regions")}
+                >
+                    Regions
+                </button>
+
+            </header>
+        )
+    }
+
     return (
         <div className="page">
-            {
+            {/*
                 (queryResults) ? <div className="blocker" onClick = { () => setQueryResults(undefined) }></div> : null
-            }
-            <div className="Widget--container">
-                <header className="Widget--header">
-                    {
-                        (user)
-                        ? <h2>Hello, { userData.firstName }</h2>
-                        : <h2>Hello</h2>
-                    }
-                    
-                    <form onSubmit = { handleSearch }
-                        className={(queryResults) ? "search-region-container search-region-container-results" : "search-region-container"}
-                    >
-                        <input 
-                            className="search-bar"
-                            type="text"
-                            placeholder="Region"
-                            value = { regionQuery }
-                            onChange = { (e: React.ChangeEvent<HTMLInputElement>) => setRegionQuery(e.target.value)}
-                        />
-                        <button type='submit' className={(regionQuery.length > 0) ? "search-btn" : "search-btn display-none"}><CiSearch /></button>
-                        {
-                            (queryResults) ? 
-                                <ul className="query-results-container">
-                                    {
-                                        queryResults.map((loc,idx) => (
-                                            <li
-                                                onClick = { () => handleSelectLocation(loc) }
-                                                key = { idx+ loc.lat }>
-                                                    {loc.name + ", " + ( (loc.state) ? loc.state : countries.getName(loc.country,"en"))}
-                                            </li>
-                                            ))
+            */}
 
-                                    }
-                                </ul>
-                        
-                                : null
-                        }
-                    </form>        
-
-                    <button
-                        className="regions-weather-button"
-                        onClick = { ()=> nav("/regions")}
-                    >
-                        Regions
-                    </button>
-
-                </header>
-                
+            <div className="Widget--container">  
+                { getHeader() }
+ 
                 <WidgetBody usersRegions = { usersRegions } currLocation = { currLocation } weather = { weather } userData={ userData } setUsersRegions={setUsersRegions} />
                 <WidgetFooter weather = { weather } units = { userData.units }/>
                 
             </div>
+
         </div>
     )
 }
